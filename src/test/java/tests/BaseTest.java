9fccf7184912e1184c27e2c3d24fca287b66f6ad
IvanKarsakov <IvanKarsakov91@gmail.com>
2025-05-12 21:36:16 +0300

    9урок

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,12 +2,17 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 import pages.*;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 public class BaseTest {
 
@@ -20,8 +25,24 @@ public class BaseTest {
     FinalConfirmPage finalConfirmPage;
     HomePage homePage;
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setup() {
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            HashMap<String, Object> chromePrefs = new HashMap<>();
+            chromePrefs.put("credentials_enable_service", false);
+            chromePrefs.put("profile.password_manager_enabled", false);
+            options.setExperimentalOption("prefs", chromePrefs);
+            options.addArguments("--incognito");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--disable-popup-blocking");
+            options.addArguments("--disable-infobars");
+            driver = new ChromeDriver(options);
+        }
+        else if (browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        }
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
--- src/test/java/tests/SortingTest.java
+++ src/test/java/tests/SortingTest.java
@@ -9,17 +9,20 @@ public class SortingTest extends BaseTest {
         loginPage.login("standard_user", "secret_sauce");
         productsPage.clickSortButton("az");
     }
+
     @Test
     public void checkSuccessSortingZA() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
         productsPage.clickSortButton("za");
+
     }
     @Test
     public void checkSuccessSortingLH() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
         productsPage.clickSortButton("lohi");
+
     }
     @Test
     public void checkSuccessSortingHL() {

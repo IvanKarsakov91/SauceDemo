3a3beabfc1e66da7396773ae0966e1c46cff9680
IvanKarsakov91 <ivankarsakov91@gmail.com>
2025-05-28 09:04:48 +0300

    dfdfd

---
+++ src/test/java/pages/Paget.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class Paget {sdfsdfsdfs
+    sdfsdfsdf
+        sdfsdfsdf
+    sdfsdfsdf
+                sdfsdfsd
+    sdfsdfsdf
+                        sdfsdf
+    sdfsdfs
+                                dfsdf
+}

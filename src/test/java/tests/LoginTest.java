ad488857e88b3162b264f221e92019ee83d087fe
IvanKarsakov91 <ivankarsakov91@gmail.com>
2025-06-02 12:41:32 +0300

    Priority test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,41 +3,49 @@ package tests;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class LoginTest extends BaseTest {
 
-    @Test(testName = "Валидная авторизация", retryAnalyzer = Retry.class,
-            groups = {"smoke"}, priority = 1)
+    @DataProvider(name = "validLoginData")
+    public Object[][] validLoginData() {
+        return new Object[][]{
+                {"standard_user", "secret_sauce", "Products"}
+        };
+    }
+
+    @DataProvider(name = "invalidLoginData")
+    public Object[][] invalidLoginData() {
+        return new Object[][]{
+                {"standard_user", "", "Epic sadface: Password is required"},
+                {"standard_user", "1243143143",
+                        "Epic sadface: Username and password do not match any user in this service"}
+        };
+    }
+
+    @Test(dataProvider = "validLoginData", testName = "Валидная авторизация",
+            retryAnalyzer = Retry.class, groups = {"smoke"}, priority = 1)
     @Epic("Авторизация")
     @Feature("Страница логина")
     @Story("Позитивный логин")
-    public void checkSuccessLogin() {
+    public void checkSuccessLogin(String username, String password, String expectedTitle) {
         loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
-        assertEquals(productsPage.getTitle(),
-                "Products",
-                "Логин не выполнен");
+        loginPage.login(username, password);
+        assertEquals(productsPage.getTitle(), expectedTitle, "Логин не выполнен");
     }
 
-    @Test(testName = "Авторизация с пустым полем пароля", groups = {"regression"}, priority = 2)
-    public void checkLoginWithEmptyPassword() {
+    @Test(dataProvider = "invalidLoginData", testName = "Авторизация с невалидными данными",
+            groups = {"regression"}, priority = 2)
+    @Epic("Авторизация")
+    @Feature("Страница логина")
+    @Story("Негативные сценарии логина")
+    public void checkFailedLogin(String username, String password, String expectedErrorMessage) {
         loginPage.open();
-        loginPage.login("standard_user", "");
-        assertEquals(loginPage.getErrorMessage(),
-                "Epic sadface: Password is required",
-                "SO BAAAAD");
-    }
-
-    @Test(testName = "Авторизация с невалидным Password", groups = {"regression"}, priority = 3)
-    public void checkLoginWithWrongPassword() {
-        loginPage.open();
-        loginPage.login("standard_user", "1243143143");
-        assertEquals(loginPage.getErrorMessage(),
-                "Epic sadface: Username and password do not match any user in this service",
-                "SO BAAAAD");
+        loginPage.login(username, password);
+        assertEquals(loginPage.getErrorMessage(), expectedErrorMessage, "Ошибка логина не совпадает");
     }
 }
 
